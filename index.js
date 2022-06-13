05992a54c1b31d3a9ac54739e07168959a3679e5
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-06-13 07:15:05 +0300

    commit tree 39f24f8d0d82b95ce49db4dc609d12376c070d0f

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, string) {
+    return drivers.filter((driver) => {
+        return driver.toLowerCase() === string.toLowerCase();
+    });
+}
+
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter((driver) => {
+        return driver.slice(0, string.length) === string;
+    });
+}
+
+function matchName(drivers, string) {
+    return drivers.filter((driver) => {
+        return driver.name === string;
+    });
+}
+
+
+
